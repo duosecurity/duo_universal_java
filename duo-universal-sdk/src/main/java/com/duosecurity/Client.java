e0d57ce911c5c8ac2bbdc22517e6dbceb25efa0b
Aaron McConnell <amcconnell@duosecurity.com>
2024-11-20 16:08:28 -0500

    Finalize the 1.2.0 release; begin 1.2.1 snapshot and update README (#38)

--- duo-universal-sdk/src/main/java/com/duosecurity/Client.java
+++ duo-universal-sdk/src/main/java/com/duosecurity/Client.java
@@ -38,7 +38,7 @@ public class Client {
 
   private static final String USER_AGENT_LIB = "duo_universal_java";
 
-  private static final String USER_AGENT_VERSION = "1.2.0";
+  private static final String USER_AGENT_VERSION = "1.2.1-SNAPSHOT";
 
   // **************************************************
   // Fields
